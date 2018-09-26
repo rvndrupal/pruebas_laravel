34087ebbfcf42827cbabe855675db2dbfb35b3e7
Rodrigo <rodrigodrupal1@gmail.com>
2018-09-26 11:24:16 -0500

    Boton de Ocultar y Mostrar Ok

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name'=> $faker->sentence,
+        'description'=> $faker->sentence,
+        'estado' => $faker->randomElement(['ACTIVO','DESACTIVADO']),
+
+    ];
+});
